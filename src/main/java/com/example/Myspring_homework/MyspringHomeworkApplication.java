21db3b8453778c2f172b7f9381a09a6826f61a42
kimgeon <kimgeon0802@gmail.com>
2026-03-10 09:31:50 +0900

    first test

---
+++ src/main/java/com/example/Myspring_homework/MyspringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.Myspring_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringHomeworkApplication.class, args);
+	}
+
+}
